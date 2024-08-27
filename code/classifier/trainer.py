afd56a0a9a3dc0a3942e80f7c3da3111e434bdd6
quim-motger <quim.motger@gmail.com>
2024-08-27 11:07:10 +0200

    refactor trainer for multiclass and binary

--- code/classifier/trainer.py
+++ code/classifier/trainer.py
@@ -1,12 +1,9 @@
-import os
+import argparse
 import numpy as np
 from datasets import load_dataset
-from dotenv import load_dotenv
 from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
 import evaluate
-
-load_dotenv()
-
+from sklearn.model_selection import train_test_split, StratifiedKFold
 
 FOLD_QTY = 10
 LABEL_QTY = 10
@@ -38,9 +35,9 @@ def load_trainer(model, trainer_args, tokenizer, train_split, test_split):
     )
 
 
-def load_trainer_args(emotion, model_name):
+def load_trainer_args(tag, model_name):
     return TrainingArguments(
-        output_dir=f'./{model_name}_{emotion}',
+        output_dir=f'./{model_name}_{tag}',
         eval_strategy="epoch",
         save_strategy="epoch",
         learning_rate=2e-5,
@@ -52,8 +49,8 @@ def load_trainer_args(emotion, model_name):
     )
 
 
-def load_tokenizer():
-    return BertTokenizer.from_pretrained(os.getenv("TOKENIZER_ID"))
+def load_tokenizer(tokenizer_id):
+    return BertTokenizer.from_pretrained(tokenizer_id)
 
 
 def preprocess(examples, tokenizer):
@@ -77,47 +74,90 @@ def preprocess(examples, tokenizer):
     }
 
 
-def train(model, tokenizer, train_split, test_split, emotion, model_name):
+def train(model, tokenizer, train_split, test_split, tag, model_name):
     trainer = load_trainer(model,
-                           load_trainer_args(emotion, model_name),
-                           tokenizer,
-                           train_split,
-                           test_split)
+                        load_trainer_args(tag, model_name),
+                        tokenizer,
+                        train_split,
+                        test_split)
     trainer.train()
     return trainer
 
 
-def train_model(model, tokenizer, dataset):
-    model_name = os.getenv("MODEL_ID").split('/')[-1]
+def train_model(model, tokenizer, split_datasets, model_name, multiclass):
     all_fold_metrics = []
-    for emotion in LABEL_MAP.keys():
-
-        train_split = dataset[f'{emotion}_train']
-        test_split = dataset[f'{emotion}_test']
-        trainer = train(model, tokenizer, train_split, test_split, emotion, model_name)
-        all_fold_metrics.append(evaluate_metrics(trainer))
-        trainer.save_model(f"./{model_name}_{emotion}")
+    if multiclass:
+        for i in range(1, FOLD_QTY):
+            train_split = split_datasets[f'train_{i}']
+            test_split = split_datasets[f'test_{i}']
+            trainer = train(model, tokenizer, train_split, test_split, i, model_name)
+            all_fold_metrics.append(evaluate_metrics(trainer))
+            #trainer.save_model(f"./{model_name}_{i}")
+    else:
+        for emotion in LABEL_MAP.keys():
+            train_split = split_datasets[f'train_{emotion}']
+            test_split = split_datasets[f'test_{emotion}']
+            trainer = train(model, tokenizer, train_split, test_split, emotion, model_name)
+            all_fold_metrics.append(evaluate_metrics(trainer))
+            #trainer.save_model(f"./{model_name}_{emotion}")
     return all_fold_metrics
 
 
-def load_hf_model():
-    return BertForSequenceClassification.from_pretrained(os.getenv("MODEL_ID"), num_labels=LABEL_QTY)
+def load_hf_model(model_id):
+    return BertForSequenceClassification.from_pretrained(model_id, num_labels=LABEL_QTY)
 
 
-def load_hf_dataset():
-    return load_dataset(os.getenv("REPOSITORY_K10_BY_SENTIMENT_ID"))
+def load_hf_dataset(repository_id):
+    return load_dataset(repository_id)
 
 
-def preprocess_dataset(dataset, tokenizer):
-    for emotion in LABEL_MAP.keys():
-        train_split = dataset[f'{emotion}_train']
-        test_split = dataset[f'{emotion}_test']
+def preprocess_dataset(dataset, tokenizer, multiclass=True):
+    def tokenize_and_process(example):
+        # Tokenize the 'review' and 'sentence' fields
+        review_tokens = tokenizer(
+            example['review'], padding='max_length', truncation=True, max_length=128, return_tensors=None
+        )
+        sentence_tokens = tokenizer(
+            example['sentence'], padding='max_length', truncation=True, max_length=128, return_tensors=None
+        )
+        
+        combined_inputs = sentence_tokens['input_ids'] + [tokenizer.sep_token_id] + review_tokens['input_ids']
+        combined_attention_mask = sentence_tokens['attention_mask'] + [1] + review_tokens['attention_mask']
+        label = LABEL_MAP.get(example['emotion-primary-agreement'], -1)
 
-        train_split = train_split.map(lambda x: preprocess(x, tokenizer), batched=True)
-        test_split = test_split.map(lambda x: preprocess(x, tokenizer), batched=True)
+        return {
+            'input_ids': combined_inputs,
+            'attention_mask': combined_attention_mask,
+            'label': label
+        }
+    
+    # Preprocess the entire dataset once
+    processed_dataset = dataset['train'].map(tokenize_and_process, batched=False)
 
-        dataset[f'{emotion}_train'] = train_split
-        dataset[f'{emotion}_test'] = test_split
+    split_datasets = {}
+
+    if multiclass:
+        # Perform k-fold cross-validation with k=10
+        kf = StratifiedKFold(n_splits=FOLD_QTY, shuffle=True, random_state=42)
+        labels = processed_dataset['label']
+
+        for fold, (train_index, test_index) in enumerate(kf.split(np.zeros(len(labels)), labels)):
+            train_split = processed_dataset.select(train_index)
+            test_split = processed_dataset.select(test_index)
+
+            split_datasets[f'train_{fold+1}'] = train_split
+            split_datasets[f'test_{fold+1}'] = test_split
+
+    else:
+        # Create train-test partitions for each emotion category
+        for emotion in LABEL_MAP.keys():
+            emotion_dataset = processed_dataset.filter(lambda x: x['label'] == LABEL_MAP[emotion])
+            train_split, test_split = train_test_split(emotion_dataset, test_size=0.2, stratify=emotion_dataset['label'])
+
+            split_datasets[f'train_{emotion}'] = train_split
+            split_datasets[f'test_{emotion}'] = test_split
+
+    return split_datasets
 
 def save_metrics_to_file(metrics, filename):
     with open(filename, 'w') as file:
@@ -129,26 +169,43 @@ def save_metrics_to_file(metrics, filename):
             file.write("\n")
 
 
-def push_model_to_hf():
-    model_name = os.getenv("MODEL_ID").split('/')[-1]
+def push_model_to_hf(model_id, tokenizer_id):
+    model_name = model_id.split('/')[-1]
     for emotion in LABEL_MAP.keys():
         print(f"{emotion} model pushed to Hugging Face Hub.")
         model = BertForSequenceClassification.from_pretrained(f"./{model_name}_{emotion}")
-        tokenizer = BertTokenizer.from_pretrained(os.getenv("TOKENIZER_ID"))
+        tokenizer = BertTokenizer.from_pretrained(tokenizer_id)
         model.push_to_hub(f"{model_name}_{emotion}")
         tokenizer.push_to_hub(f"{model_name}_{emotion}")
 
 
-def main():
-    dataset = load_hf_dataset()
-    model = load_hf_model()
-    tokenizer = load_tokenizer()
-    preprocess_dataset(dataset, tokenizer)
+def main(args):
+    # Set arguments
+    dataset = load_hf_dataset(args.repository_id)
+    model = load_hf_model(args.model_id)
+    tokenizer = load_tokenizer(args.tokenizer_id)
+    multiclass = args.multiclass
+    
+    # Split and tokenize dataset
+    split_datasets = preprocess_dataset(dataset, tokenizer, multiclass)
 
-    metrics = train_model(model, tokenizer, dataset)
+    # Train model
+    metrics = train_model(model, tokenizer, split_datasets, args.model_id.split('/')[-1], multiclass)
+    
+    # Save metrics
     save_metrics_to_file(metrics, 'metrics.txt')
-    push_model_to_hf()
+    
+    # Push model to HuggingFace
+    #push_model_to_hf(args.model_id, args.tokenizer_id)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Model Training Parameters")
+
+    parser.add_argument("--model-id", required=True, help="Hugging Face model ID")
+    parser.add_argument("--tokenizer-id", required=True, help="Hugging Face tokenizer ID")
+    parser.add_argument("--repository-id", required=True, help="Hugging Face dataset repository ID")
+    parser.add_argument("--multiclass", required=True, help="Multiclass (True) or binary (False) classification")
+
+    args = parser.parse_args()
+    main(args)
